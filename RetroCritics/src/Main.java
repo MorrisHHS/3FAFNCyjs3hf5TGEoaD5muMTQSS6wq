5ab343ea4ffc07831e84293817739ae203d3d346
William <vooijs54@gmail.com>
2024-04-04 14:35:53 +0200

    UwU

--- RetroCritics/src/Main.java
+++ RetroCritics/src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        for (int value = 0; value < 10000; value++){
 
-            SaveManager.writeToFile("UwU OwO QwQ TwT", "C:\\Users\\vooij\\IdeaProjects\\3FAFNCyjs3hf5TGEoaD5muMTQSS6wq\\RetroCritics\\FileTest\\UwU" + value + ".txt");
-        }
     }
 }
