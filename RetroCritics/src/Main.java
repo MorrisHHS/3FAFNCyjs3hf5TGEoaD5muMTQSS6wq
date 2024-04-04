a0acce6fb5ce06d9ba35ad90903230c31531e5eb
William <vooijs54@gmail.com>
2024-04-04 14:31:36 +0200

    UwU

--- RetroCritics/src/Main.java
+++ RetroCritics/src/Main.java
@@ -1,7 +1,8 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        
+        for (int value = 0; value < 10000; value++){
+
+            SaveManager.writeToFile("UwU OwO QwQ TwT", "C:\\Users\\vooij\\IdeaProjects\\3FAFNCyjs3hf5TGEoaD5muMTQSS6wq\\RetroCritics\\FileTest\\UwU" + value + ".txt");
+        }
     }
 }
