831ed7c50c6aa14af7db3a534fbb8d764da993eb
xande <23096780@student.hhs.nl>
2024-04-05 11:16:44 +0200

    nigger ding

--- RetroCritics/src/GameAspect.java
+++ RetroCritics/src/GameAspect.java
@@ -1,7 +1,12 @@
 public class GameAspect {
+    String naam;
     public double beoordeling;
 
-    public GameAspect(double beoordeling) {
+    public GameAspect(String naam) {
+        this.naam = naam;
+    }
+    public GameAspect(String naam, double beoordeling) {
+        this.naam = naam;
         this.beoordeling = beoordeling;
     }
 }
--- RetroCritics/src/Main.java
+++ RetroCritics/src/Main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        SaveManager.writeToFile("UwU OwO QwQ TwT", "C:\\Users\\vooij\\IdeaProjects\\3FAFNCyjs3hf5TGEoaD5muMTQSS6wq\\RetroCritics\\FileTest\\UwU.txt");
+        Scanner scanner = new Scanner(System.in);
+        Game Famctorio = new Game("Famctorio", 1000);
+        Famctorio.maakReviewAan(scanner);
 
     }
 }
