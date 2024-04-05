67c7dab7073c01e7f533a63bc7b15b3e8d1cdfa9
Morris <23085975@student.hhs.nl>
2024-04-05 09:41:15 +0200

    amogus

--- RetroCritics/src/Menu.java
+++ RetroCritics/src/Menu.java
@@ -7,13 +7,18 @@ public class Menu {
 Scanner scanner = new Scanner (System.in);
 int keuze = 0;
 
-while(keuze !8) {
+while(keuze !4) {
     System.out.println("Menu:");
+    System.out.println("Maak een keuze");
     System.out.println("1. Games bekijken");
     System.out.println("2. Game reviewen");
-    System.out.println("3. ");
-    System.out.println("4. ");
+    System.out.println("3. Uitverkoop");
+    System.out.println("4. Afsluiten");
 
+    if(scanner.hasNextInt()) {
+        keuze = scanner.nextInt();
+        scanner.nextLine();
+    }
         }
     }
     public void voegKeuzeToe(Menukeuze menu){
