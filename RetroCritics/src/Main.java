8f6eabd019e48c01b7bb1e05bec05831cc262c6f
VipapkStudiosOfficial <paragh.abhishek@outlook.com>
2024-04-09 14:15:25 +0200

    EnqueteMain_commit_2

--- RetroCritics/src/Main.java
+++ RetroCritics/src/Main.java
@@ -1,6 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
-        SaveManager.writeToFile("UwU OwO QwQ TwT", "C:\\Users\\vooij\\IdeaProjects\\3FAFNCyjs3hf5TGEoaD5muMTQSS6wq\\RetroCritics\\FileTest\\UwU.txt");
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args)
+    {
+        //EnqueteAbhi
+        Enquete enquete = new Enquete();
+        EnqueteVragen enqueteVragen = new EnqueteVragen();
+
+        // Get questions from EnqueteVragen and add them to Enquete
+        ArrayList<String> vragen = EnqueteVragen.getVragen();
+        for (String vraag : vragen) {
+            enquete.voegVraagToe(vraag);
+        }
+
+        // Display the questions
+        System.out.println(enquete.toonVragen());
     }
 }
