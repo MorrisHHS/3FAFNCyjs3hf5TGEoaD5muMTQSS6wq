f570cfc63f1797f7a9b851fdb8c5d910b24d5784
Melle <23124024@student.hhs.nl>
2024-04-09 13:24:46 +0200

    ja

---
+++ RetroCritics/src/SaveData.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class SaveData {
+    private Map<String, String> data = new HashMap<>();
+
+    public void addAttribute(String key, String value) {
+        data.put(key, value);
+    }
+    public String readAtrribute(String key){
+        return data.get(key);
+    }
+    public String getData (){
+        Set<String> keys = data.keySet();
+        String s = "";
+        for(String key : keys) {
+            s += key;
+            s += "=";
+            s += data.get(key);
+            s += ":";
+        }
+        return s;
+
+    }
+    public void setData(String data) {
+        String[] splitData = data.split(":");
+        for (String s : splitData) {
+            String[] finaleData = s.split("=");
+            this.data.put(finaleData[0], finaleData[1]);
+        }
+
+    }
+}
