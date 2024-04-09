a4c7e1a2692748ed4c751d4928eeed6402440c32
Morris <23085975@student.hhs.nl>
2024-04-09 15:08:27 +0200

    amogus

--- RetroCritics/src/Main.java
+++ RetroCritics/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        SaveManager.writeToFile("UwU OwO QwQ TwT", "C:\\Users\\vooij\\IdeaProjects\\3FAFNCyjs3hf5TGEoaD5muMTQSS6wq\\RetroCritics\\FileTest\\UwU.txt");
-
+        MenuTest.Run();
     }
 }
