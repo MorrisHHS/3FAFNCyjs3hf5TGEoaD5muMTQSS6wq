04a07e8ebf5f411e396b16d9d20afd7949f67c93
VipapkStudiosOfficial <paragh.abhishek@outlook.com>
2024-04-09 13:24:55 +0200

    Enquete_commit_4

--- RetroCritics/src/Enquete.java
+++ RetroCritics/src/Enquete.java
@@ -1,19 +1,24 @@
 import java.util.ArrayList;
 
-public class Enquete {
+public class Enquete
+{
     private ArrayList<Vraag> vragen;
 
-    public Enquete() {
+    public Enquete()
+    {
         this.vragen = new ArrayList<>();
     }
 
-    public void voegVraagToe(Vraag vraag) {
+    public void voegVraagToe(Vraag vraag)
+    {
         this.vragen.add(vraag);
     }
 
-    public String toonVragen() {
+    public String toonVragen()
+    {
         StringBuilder result = new StringBuilder();
-        for (Vraag vraag : vragen) {
+        for (Vraag vraag : vragen)
+        {
             result.append(vraag.toString()).append("\n");
         }
         return result.toString();
