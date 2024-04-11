066e3c51a0e24180d6bd9f23c3f094b84310bb4b
William <vooijs54@gmail.com>
2024-04-11 15:48:52 +0200

    Added loadGame and loadAllGames methods to the save manigger. manager*

--- RetroCritics/src/Main.java
+++ RetroCritics/src/Main.java
@@ -1,10 +1,22 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
         SaveData game = new SaveData();
         game.addAttribute("Naam", "Factorio");
-        game.addAttribute("Prijs", "3000000");
+        game.addAttribute("Prijs", "30000");
         SaveManager.saveGame(game, "Famctorio");
+
+        SaveData game2 = new SaveData();
+        game2.addAttribute("Naam", "Skyrim");
+        game2.addAttribute("Prijs", "30");
+        SaveManager.saveGame(game2, "Skyrim");
+
+        SaveData game3 = new SaveData();
+        game3.addAttribute("Naam", "Call_Of_Duty");
+        game3.addAttribute("Prijs", "70");
+        SaveManager.saveGame(game3, "Call_Of_Duty");
 
         SaveData review = new SaveData();
         review.addAttribute("Titel", "Mijn Eerste Review");
@@ -16,5 +28,11 @@ public class Main {
         enqt.addAttribute("Wat geef ik de app als rating?", "9/11");
         SaveManager.saveEnquete(enqt);
 
+        ArrayList<SaveData> games = SaveManager.loadAllGames();
+        for (SaveData gam : games){
+            System.out.println(gam.readAttribute("Naam"));
+            System.out.println(gam.readAttribute("Prijs"));
+            System.out.println();
+        }
     }
 }
