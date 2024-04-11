c6bbed3727671af49bcd2c5ebaaddb688447ce1f
Morris <23085975@student.hhs.nl>
2024-04-11 14:04:11 +0200

    amogus

--- RetroCritics/src/MainMenu.java
+++ RetroCritics/src/MainMenu.java
@@ -7,16 +7,25 @@ public class MainMenu extends Menukeuze
     @Override
     public void toonMenukeuze()
     {
+
         String[] spaceInvader =
         {
-                "  ▄▄███▄▄  ",
-                "▄█▀█████▀█▄",
-                "██▄▀███▀▄██",
-                "▀▀██▀▀▀██▀▀"
+                "  ▄▄███▄▄     ",
+                "▄█▀█████▀█▄   ",
+                "██▄▀███▀▄██   ",
+                "▀▀██▀▀▀██▀▀   "
         };
 
+
         for (int i = 0; i < spaceInvader.length; i++) {
-            System.out.println(spaceInvader[i]);
+            for (int j = 0; j < 3; j++){
+                System.out.print(spaceInvader[i]);
+            }
+            System.out.println();
         }
+
+        System.out.println(" ");
+        System.out.println("Je zit in menu: " + naam);
+        menu.toonMenu();
     }
 }
