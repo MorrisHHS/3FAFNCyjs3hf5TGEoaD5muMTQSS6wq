aed0c669bdb054eb5391321a301b3b35891ecf9c
Morris <23085975@student.hhs.nl>
2024-04-11 13:55:17 +0200

    amogus

--- RetroCritics/src/Main.java
+++ RetroCritics/src/Main.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Game valurFamctorio = new Game("Famctorio", 1000);
-        Review review1 = new Review(scanner);
+        MenuTest.Run();
     }
 }
