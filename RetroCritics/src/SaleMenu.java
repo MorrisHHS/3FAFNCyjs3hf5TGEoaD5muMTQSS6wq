a1048e1c108b8759052076dda13ac9b5d366477b
VipapkStudiosOfficial <paragh.abhishek@outlook.com>
2024-04-12 13:37:07 +0200

    commit

--- RetroCritics/src/SaleMenu.java
+++ RetroCritics/src/SaleMenu.java
@@ -1,4 +1,4 @@
-public class SaleMenu extends Menukeuze{
+public class SaleMenu extends Menukeuze {
 
     public SaleMenu(String naam) {
         super(naam);
@@ -8,8 +8,22 @@ public class SaleMenu extends Menukeuze{
     public void toonMenukeuze() {
         System.out.println();
 
-        // gooi hier de games met korting in
+        // Kortingspercentage instellen
+        int kortingBedrag = 20; // 20% korting (in centen)
 
+        System.out.println("Games met korting:");
+        for (Game game : Game.list) {
+            // Korting berekenen en toepassen
+            int korting = (game.getPrijs() * kortingBedrag) / 100;
+            int nieuwePrijs = game.getPrijs() - korting;
+
+            // Afdrukken van game met korting
+            System.out.println("Game naam: " + game.getNaam());
+            System.out.println("Oude prijs: " + game.getPrijs());
+            System.out.println("Korting: " + korting);
+            System.out.println("Nieuwe prijs na korting: " + nieuwePrijs);
+            System.out.println();
+        }
 
         System.out.println(" ");
         System.out.println(naam + ":");
