3c5b03423ab5412e6c596580ce4f736631b79e72
MorrisHHS <23085975@student.hhs.nl>
2024-04-12 11:52:59 +0200

    amogus

--- RetroCritics/src/Main.java
+++ RetroCritics/src/Main.java
@@ -4,6 +4,21 @@ public class Main {
     public static void main(String[] args) {
         SaveManager.initialize(); // DO NOT REMOVE DIS LINE BITCH
 
+        Menukeuze mainMenu = new MainMenu("Main Menu");
+        Menukeuze gameListMenu = new GameListMenu("Game List Menu");
+        Menukeuze gameMenu = new GameMenu("Game Menu");
+        Menukeuze saleMenu = new SaleMenu("Sale Menu");
+
+        mainMenu.menu.voegKeuzeToe(gameListMenu);
+        mainMenu.menu.voegKeuzeToe(saleMenu);
+        gameListMenu.menu.voegKeuzeToe(gameMenu);
+        gameListMenu.menu.voegKeuzeToe(mainMenu);
+        gameListMenu.menu.voegKeuzeToe(saleMenu);
+        gameMenu.menu.voegKeuzeToe(mainMenu);
+        gameMenu.menu.voegKeuzeToe(gameListMenu);
+
+        mainMenu.toonMenukeuze();
+
         for (Game g : Game.list){
             System.out.println("Naam : " + g.naam);
             System.out.println("Genre: " + g.genre);
