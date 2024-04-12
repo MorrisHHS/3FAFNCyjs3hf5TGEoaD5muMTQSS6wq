0749a4a8c1f5c9afea3dec1c0c944c36ce69c44a
Melle <23124024@student.hhs.nl>
2024-04-12 10:05:01 +0200

    ja

--- RetroCritics/src/GameListMenu.java
+++ RetroCritics/src/GameListMenu.java
@@ -1,4 +1,6 @@
-public class GameListMenu extends Menukeuze{
+import java.util.Scanner;
+
+public class GameListMenu extends Menukeuze {
 
     public GameListMenu(String naam) {
         super(naam);
@@ -6,10 +8,37 @@ public class GameListMenu extends Menukeuze{
 
     @Override
     public void toonMenukeuze() {
-        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        String antwoord = scanner.nextLine();
+        System.out.println("Wilt u een genre kiezen?(y/n)");
 
-        // gooi hier de games in
-        //Savemanager.load
+        if (antwoord.equals("y")) {
+            for (Game game : Game.list) {
+                System.out.println("Game naam: " + game.naam);
+                System.out.println("Prijs: €" + game.prijs);
+                System.out.println();
+            }
+        }
+        else {
+            System.out.println("Kies een genre: \n 1. RPG \n 2. Puzzel \n 3. Strategie");
+            for (Game game : Game.list) {
+                int keuze = scanner.nextInt();
+                switch (keuze){
+                    case 1:
+                        if(game.genre.equals("RPG")) {
+                            System.out.println(game.genre);
+                        }
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
 
         System.out.println(" ");
         System.out.println(naam + ":");
