47508fa7c6438762b222025356c16e202ecf2cae
William <vooijs54@gmail.com>
2024-04-12 12:41:57 +0200

    gam

--- RetroCritics/src/Main.java
+++ RetroCritics/src/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         SaveManager.initialize(); // DO NOT REMOVE DIS LINE BITCH
 
+        // Dit print alle gams en refieuws. Dit mag u weg halen als u daar voldoening aan doet
         for (Game g : Game.list){
             System.out.println("Naam : " + g.naam);
             System.out.println("Genre: " + g.genre);
             System.out.println("Prijs: " + g.prijs);
-            System.out.println();
             System.out.println("Reviews: ");
 
             for (Review r : g.reviews){
@@ -17,6 +17,8 @@ public class Main {
                 System.out.println(r.toelichting);
                 System.out.println();
             }
+            System.out.println();
+            System.out.println();
         }
     }
 }
